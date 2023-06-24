8010be0f573b136914c072c31e907e0308a34b28
Nav-33n <105544168+Nav-33n@users.noreply.github.com>
2023-06-24 16:31:55 +0530

    hyperlink

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,11 @@
 export default function Footer() {
     return (
         <footer>
-            <i class="fa-brands fa-instagram"></i>
-            <i class="fa-brands fa-twitter"></i>
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-brands fa-discord"></i>
+            <a href="https://www.instagram.com/naveenx42/" rel="noreferrer" alt="instagram" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://twitter.com/howtotweet42" rel="noreferrer" alt="twitter" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://github.com/Nav-33n" rel="noreferrer" alt="github" target="_blank"><i class="fa-brands fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/nabin-sharma-8748a7229/" rel="noreferrer" alt="Linkedin" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://discord.com/channels/@me" rel="noreferrer" alt="discord" target="_blank"><i class="fa-brands fa-discord"></i></a>
         </footer>
     )
 }
